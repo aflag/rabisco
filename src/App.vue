58173a041c37f06bfe4bd860c996198679231ccd
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2018-10-28 17:14:19 +0000

    Create app backbone

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <notifications position="top center" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  margin: auto;
+  max-width: 1200px;
+}
+</style>
