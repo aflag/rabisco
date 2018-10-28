09284dde1242f8171243f5b7eb0eb5447421f930
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2018-10-28 19:09:17 +0000

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-    <notifications position="top center" />
+    <notifications position="top center"/>
     <TheHeader/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import TheHeader from '@/components/TheHeader.vue'
+    import TheHeader from '@/components/TheHeader.vue';
 
-export default {
-  name: 'app',
-  components: {
-    TheHeader
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TheHeader
+        }
+    };
 </script>
 
 <style>
-#app {
-  margin: auto;
-  max-width: 1200px;
-  font-family: 'Open Sans', sans-serif;
-}
+  #app {
+    margin: auto;
+    max-width: 1200px;
+    font-family: 'Open Sans', sans-serif;
+  }
 </style>
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -3,36 +3,36 @@
     <img alt="Loading" src="../assets/loading.gif">
   </div>
   <div v-else class="room">
-    <Game :room="room" />
+    <Game :room="room"/>
   </div>
 </template>
 
 <script>
-import Game from '@/components/Game.vue'
-import rabisco from '@/rabisco.js'
+    import Game from '@/components/Game.vue';
+    import rabisco from '@/rabisco.js';
 
-export default {
-  name: 'room',
-  components: {
-    Game
-  },
-  data() {
-     return {
-        room: null,
-     }
-  },
-  created() {
-    var loadData = () => {
-      rabisco.getRoom(this.$route.params.id)
-        .then(response => this.room = response.data)
-    }
-    loadData()
-    setInterval(loadData, 15000)
-  }
-}
+    export default {
+        name: 'room',
+        components: {
+            Game
+        },
+        data() {
+            return {
+                room: null,
+            };
+        },
+        created() {
+            var loadData = () => {
+                rabisco.getRoom(this.$route.params.id)
+                  .then(response => this.room = response.data);
+            };
+            loadData();
+            setInterval(loadData, 15000);
+        }
+    };
 </script>
 <style scoped>
-.waiting {
-  text-align: center;
-}
+  .waiting {
+    text-align: center;
+  }
 </style>
